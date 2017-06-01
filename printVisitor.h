5f9d65c7f1c01e962e4423a8ac528a78f44d3a09
Jonathan Woolf <jwool003@ucr.edu>
2017-05-31 19:01:47 -0700

    added visitor base class

--- printVisitor.h
+++ printVisitor.h
@@ -1,6 +1,20 @@
 #ifndef __PRINTVISITOR_H__
 #define __PRINTVISITOR_H__
 
+class Visitor {
+	private:
+		std::string output;
+
+	public:
+		virtual void rootNode() {}		//For visiting a root node (do nothing)
+		virtual void sqrNode() {}		    //For visiting a square node
+		virtual void multNode() {}		  //For visiting a multiple node
+		virtual void subNode() {}		    //For visiting a subtraction node
+		virtual void addNode() {}		    //For visiting an add node
+		virtual void opNode(Op* op) {}	//For visiting a leaf node
+		virtual void execute() {}		    //Prints all visited nodes
+};
+
 class PrintVisitor : public Visitor {
 	private:
 		std::string output;
